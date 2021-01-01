d140bb930f7154e3e60fa80d83abc7be664af46b
IoanCojo <ioana.cojocaru.a@gmail.com>
2021-01-01 19:50:48 +0000

    add Track to playList

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -12,7 +12,8 @@ class TrackList extends React.Component {
             {
                 this.props.tracks.map( track => {
                     return <Track track={track} 
-                                    key={track.id}/>
+                                    key={track.id}
+                                    onAdd={this.props.onAdd}/>
                 })
             }  
             </div>
